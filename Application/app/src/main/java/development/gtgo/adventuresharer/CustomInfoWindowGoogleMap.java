b1fcd6db21d13e1ef1eb9232c82f66ddc312986d
Caleb Toi <calebtjtoi@gmail.com>
2018-10-23 03:07:53 +1300

    Update CustomInfoWindowAdapter functionality
    Added some comments to the document at well

--- Application/app/src/main/java/development/gtgo/adventuresharer/CustomInfoWindowGoogleMap.java
+++ Application/app/src/main/java/development/gtgo/adventuresharer/CustomInfoWindowGoogleMap.java
@@ -3,65 +3,66 @@ package development.gtgo.adventuresharer;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.net.Uri;
-import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.DataSource;
-import com.bumptech.glide.load.engine.GlideException;
-import com.bumptech.glide.request.RequestListener;
-import com.bumptech.glide.request.target.Target;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
 
 
+/**
+ *  Customised adapter for Google Map's built in InfoWindow
+ *
+ *  This has been changed so that it will display an image as well as the title and description
+ *  of the Point of Interest (POI).
+ *
+ */
 public class CustomInfoWindowGoogleMap implements GoogleMap.InfoWindowAdapter {
 
-
     private Context context;
+
 
     public CustomInfoWindowGoogleMap(Context context){
         this.context = context;
     }
 
+    // Needed as Class implements InfoWindowAdapter
     @Override
     public View getInfoWindow(Marker marker) {
         return null;
     }
 
+    /**
+     *  Overrides the original InfoWindow function to implement a way to populate the View
+     *  This functions takes all the information stored in the Marker object to populate
+     *  the Views within this Custom InfoWindow.
+     *
+     * @param marker
+     * @return view
+     */
     @Override
     public View getInfoContents(Marker marker) {
 
+        // Creates View object based on the custom_info_window layout file
         View view = ((Activity)context).getLayoutInflater().inflate(R.layout.custom_info_window, null);
 
+        // Instantiates the Text and Image Views within the Layout
         TextView name = view.findViewById(R.id.infoWindowName);
         TextView desc = view.findViewById(R.id.infoWindowSnippet);
         ImageView img = view.findViewById(R.id.infoWindowImage);
 
+        // Retrieves the Image from the Marker and sets it to the ImageView
         if(marker.getTag() != null ) {
-//            String imgURI = marker.getTag().toString();
-//            img.setImageURI(Uri.parse(imgURI));
-//            Uri imgURI = Uri.parse(marker.getTag().toString());
-
             Bitmap bm = (Bitmap) marker.getTag();
             img.setImageBitmap(bm);
-
-            Toast.makeText(view.getContext(), "RECEIVING TAG URI", Toast.LENGTH_LONG).show();
-
-
-
-
         }
 
-
+        // Sets TextViews
         name.setText(marker.getTitle());
         desc.setText(marker.getSnippet());
-;
+
+        // Returns the created View object
         return view;
     }
 }
