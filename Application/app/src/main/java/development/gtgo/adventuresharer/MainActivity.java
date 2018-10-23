448453d06e5b4c04bb3628d5d5b6eee25acf41ff
Caleb Toi <calebtjtoi@gmail.com>
2018-10-23 15:15:24 +1300

    Update Main Activity code

--- Application/app/src/main/java/development/gtgo/adventuresharer/MainActivity.java
+++ Application/app/src/main/java/development/gtgo/adventuresharer/MainActivity.java
@@ -23,15 +23,20 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.OnL
     String loginEmail;
     String loginPassword;
 
-    String signupUsername;
-    String signupEmail;
-    String signupPassword;
+    String signUpEmail;
+    String signUpPassword;
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
     private String TAG = "1234";
 
-
+    /**
+     *  Takes in an email and password from the login fragment and authenticates it again the information
+     *  held on the database side
+     *
+     * @param email
+     * @param password
+     */
     @Override
     public void OnLoginDataPasser(String email, String password){
 
@@ -39,7 +44,7 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.OnL
         loginPassword = password;
 
         if(loginEmail != null && loginPassword != null){
-
+            // FireBase login process
             mAuth.signInWithEmailAndPassword(loginEmail, loginPassword)
                     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                         @Override
@@ -47,8 +52,6 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.OnL
                             if (task.isSuccessful()) {
                                 // Sign in success
                                 Log.d(TAG, "signInWithEmail:success");
-                                FirebaseUser user = mAuth.getCurrentUser();
-
                                 Toast.makeText(MainActivity.this, "Logged in!",
                                         Toast.LENGTH_SHORT).show();
                             } else {
@@ -63,12 +66,19 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.OnL
         }
     }
 
+    /**
+     * This function takes in an email and password from the Sign Up Fragment and creates
+     * and account based on that information.
+     *
+     * @param email
+     * @param password
+     */
     @Override
     public void OnSignUpDataPasser(String email, String password){
-        signupEmail = email;
-        signupPassword = password;
-
-        if(signupEmail != null && signupPassword != null){
+        signUpEmail = email;
+        signUpPassword = password;
+        if(signUpEmail != null && signUpPassword != null){
+            // FireBase Sign Up process
             mAuth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                         @Override
@@ -76,15 +86,12 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.OnL
                             if (task.isSuccessful()) {
                                 // Sign in success, update UI with the signed-in user's information
                                 Log.d(TAG, "createUserWithEmail:success");
-                                FirebaseUser user = mAuth.getCurrentUser();
                             } else {
                                 // If sign in fails, display a message to the user.
                                 Log.w(TAG, "createUserWithEmail:failure", task.getException());
                                 Toast.makeText(MainActivity.this, "Account creation failed! Please try again..",
                                         Toast.LENGTH_SHORT).show();
                             }
-
-                            // ...
                         }
                     });
         }
@@ -96,7 +103,7 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.OnL
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
-
+        // Creates an auth listener to ensure that the user is still signed in
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -126,13 +133,14 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.OnL
     @Override
     protected void onResume() {
         super.onResume();
+        // Add the Auth listener when the activity is in focus
         mAuth.addAuthStateListener(mAuthStateListener);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-
+        // Removes the Auth listener when the activity is out of focus
         mAuth.removeAuthStateListener(mAuthStateListener);
     }
 }
